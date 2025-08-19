712a930bfd7defce9615d8d0e27b9a3cace32c17
Venu Kailash <venu.sambasivam@cmrsurgical.com>
2025-08-19 14:41:59 +0100

    detailed logging

--- tests/runner.ai.ts
+++ tests/runner.ai.ts
@@ -16,15 +16,38 @@ async function main() {
   const response = await ai.models.generateContentStream({
     model: "gemini-2.5-flash",
     config: {
-      tools: [mcpToTool(playwrightClient)],
+      tools: [mcpToTool(playwrightClient)], // Inject Playwright MCP Client as Tool
     },
     contents: prompt,
   });
+
   for await (const chunk of response) {
-    if (chunk.text) {
-      process.stdout.write(chunk.text);
+    if (chunk.functionCalls && chunk.functionCalls.length > 0) {
+      for (const functionCall of chunk.functionCalls) {
+        console.log(`Function to call: ${functionCall.name}`);
+        console.log(`Arguments: ${JSON.stringify(functionCall.args)}`);
+      }
+    }
+
+    const candidates = chunk.candidates || [];
+    for (const candidate of candidates) {
+      if (candidate.content?.parts) {
+        for (const part of candidate.content.parts) {
+          if (part.text) console.log(`Text part : ${part.text}`);
+          if (part.thought) console.log(`Thought: ${part.thought}`);
+          if (part.executableCode && part.executableCode.code) {
+            console.log(`Executable Code: ${part.executableCode.code}`);
+          }
+          if (part.codeExecutionResult && part.codeExecutionResult.output) {
+            console.log(
+              `Code Execution Output: ${part.codeExecutionResult.output}`
+            );
+          }
+        }
+      }
     }
   }
+
   await playwrightClient.close();
 }
 
--- tests/runner.utils.ts
+++ tests/runner.utils.ts
@@ -6,12 +6,14 @@ import path from "path";
 import "dotenv/config";
 
 export const getAllFeatures = (): string => {
+  // Read all feature files
   const __dirname = fileURLToPath(import.meta.url);
   const featureFolder = path.join(__dirname, "../features");
   const features = fs.readdirSync(featureFolder);
   let allFeatures = "";
   console.log(`Starting test for ${features.length} features`);
 
+  // Append all features into a single feature for prompt
   for (const featureFile of features) {
     const filePath = path.join(featureFolder, featureFile);
 
@@ -25,13 +27,17 @@ export const getAllFeatures = (): string => {
 
 export const getPlaywrightClient = () => {
   const defaultArgs = ["-y", "@playwright/mcp@latest"];
-   defaultArgs.push("--headless");
 
+  // Runs headless in CI
+  if (process.env.CI) defaultArgs.push("--headless");
+
+  // Setup params for MCP server
   const playwrightParams = new StdioClientTransport({
     command: "npx",
     args: defaultArgs,
   });
 
+  // Return Playwright MCP Client
   const playwrightClient = new Client({
     name: "playwright-mcp",
     version: "1.0.0",
